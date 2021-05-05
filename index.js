8cb34ea39b836e33738efd7ef7d666ceba16c0e4
pacificsuraj <stsurajst@gmail.com>
2021-05-05 08:27:35 +0545

    query service sync

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-
+const axios = require("axios");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 const posts = {};
-
 // EXAMPLE OF POST
 
 /* 
@@ -38,24 +37,48 @@ posts = {
 so on ...
 */
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
+const handleEvents = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
 
     posts[id] = { id, title, comments: [] };
   }
   if (type === "CommentCreated") {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
     const post = posts[postId];
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
-  console.log(posts);
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+    const post = posts[postId];
+
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  handleEvents(type, data);
+
   res.send({});
 });
 
-app.listen("4002", () => console.log("Listening on 4002"));
+app.listen("4002", async () => {
+  console.log("Listening on 4002");
+
+  const res = await axios.get("http://localhost:4005/events");
+
+  for (let event of res.data) {
+    console.log("Processing event:", event.type);
+    handleEvents(event.type, event.data);
+  }
+});
